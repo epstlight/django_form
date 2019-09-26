25bae43fd5f1b97103a35a11600a00436c0f99b3
강희원 <heewkk@gmail.com>
2019-09-26 11:34:38 +0900

    Learn Django-form

--- articles/urls.py
+++ articles/urls.py
@@ -1,5 +1,10 @@
 from django.urls import path
+from . import views
+
+app_name='articles'
 
 urlpatterns = [
-    path('', view.index)
+    path('', views.index, name='index'),
+    path('create/', views.create, name='create'),
+    path('<int:article_pk>/', views.detail, name='detail'),
 ]
--- articles/views.py
+++ articles/views.py
@@ -1,6 +1,32 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Article
+from .forms import ArticleForm
 # Create your views here.
 
 def index(request):
-    
+    articles = Article.objects.all()
+    context = {'articles': articles}
+    return render(request, 'articles/index.html', context)
+
+
+def create(request):
+    if request.method == 'POST':
+        form = ArticleForm(request.POST)
+        # title = request.POST.get('title')
+        # content = request.POST.get('content')
+        if form.is_valid():
+            title = form.cleaned_data.get('title')
+            content = form.cleaned_data.get('content')
+            article = Article(title=title, content=content)
+            article.save()
+            return redirect('articles:index')
+    else:
+        form = ArticleForm()
+    context = {'form':form}
+    return render(request, 'articles/create.html', context)
+
+
+def detail(request, article_pk):
+    article = get_object_or_404(Article, pk=article_pk)
+    context = {'article':article}
+    return render(request, 'articles/detail.html', context)
